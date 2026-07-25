9707e505f665054face1392dce7d39c210853897
scapehunter <67822248+scapehunter@users.noreply.github.com>
2026-07-25 06:19:25 +0000

    modified:   streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit as st
 st.set_page_config(page_title="CHE Operational Dashboard", layout="wide")
 
 home = st.Page("pages/home.py", title="Home", icon="🏠", default=True)
+st.image('https://curioushathi.com/wp-content/uploads/2025/05/Curious-Hathi-Logo.png')
 ticket_extractor = st.Page("pages/ticket_extractor.py", title="Ticket Extractor", icon="🎫")
 
 # Add future tools to this list as you build them, e.g.:
